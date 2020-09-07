7b0f4fb5db2a12ed9693e0efa94bfa8699165c71
Mohammad S Hussain <m.s.hussain@outlook.com>
2020-09-07 00:33:42 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,11 +1,11 @@
-count = 10 
-magic_exit_number = 0
-while count = magic_exit_number do
-  puts "Happy New Year!" && count != magic_exit_number
-  puts #{count}
-  count = count - 1 
-end
-end
+# count = 10 
+# magic_exit_number = 0
+# while count = magic_exit_number do
+#   puts "Happy New Year!" && count != magic_exit_number
+#   puts #{count}
+#   count = count - 1 
+# end
+# end
 
 count = 10
 while count >= 1 do
